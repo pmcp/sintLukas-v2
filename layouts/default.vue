6dcba8c4f0f4dbf8a7f55b4ca90c25e03e87f5c4
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 23:59:38 +0200

    pivot to build

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <main-header />
+<!--    <main-header />-->
     <UPage>
       <UPageBody :ui="{ wrapper: 'mt-0'}">
         <slot />
       </UPageBody>
     </UPage>
-    <main-footer />
+<!--    <main-footer />-->
   </div>
 </template>
