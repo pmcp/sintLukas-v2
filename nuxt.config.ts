ee8f40aae560a7dc383a4058f1244c18573569fc
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-11 00:19:34 +0200

    pivot to build 6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/content', "@nuxt/image", '@nuxt/ui'],
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicG1jcCIsImEiOiJjbHduc3hkMGExdzNwMmpteWh2M3c3cHZ3In0.4edOOiqp_E93TDvm-1BxCQ'
+  },
+  build: {
+    transpile: ['@egjs/vue-grid']
+  },
   extends: ['@nuxt/ui-pro'],
   ui: {
     safelistColors: ['black', 'primary', 'secondary']
--- pages/[page].vue
+++ pages/[page].vue
@@ -1,6 +1,11 @@
 <template>
-  here we go again - {{ data}}
-  ==== {{ error }}
+  <div class="py-8 mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+    <h1 class="text-xl font-bold mb-4">{{ data.title }}</h1>
+    <elements-intro>
+      {{ data.intro }}
+    </elements-intro>
+  </div>
+  <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>
 </template>
 <script setup lang="ts">
 const route = useRoute()
