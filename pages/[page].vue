48638b3787aeedaac312697c376fbe229e9bec91
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 23:10:54 +0200

    test generate 10

--- pages/[page].vue
+++ pages/[page].vue
@@ -1,10 +1,9 @@
 <script setup>
 const route = useRoute()
 const lang = ref('nl')
-const path = ref(`${route.path}`)
-console.log(path.value)
+const path = ref(route.path)
 const { data, error  } = await useAsyncData('page', () => queryContent(path.value).findOne())
-console.log(error, data.value)
+console.log(error, data)
 </script>
 
 <template>
