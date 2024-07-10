14864d57a43b752c7ae5fbafb4cfc073dfac8295
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 22:20:48 +0200

    added links to test generate

--- index.vue
+++ index.vue
@@ -1,5 +1,7 @@
 <template>
   HOME
+  <nuxt-link to="/pages/testpage-1">test</nuxt-link>
+  <nuxt-link to="/testpage-1">test</nuxt-link>
 <!--  <ContentList path="/" v-slot="{ list }">-->
 <!--    <div v-for="article in list" :key="article._path">-->
 <!--      <ULink :to="article._path">{{ article.title }}</ULink>-->
@@ -17,3 +19,5 @@
 <!--});-->
 
 <!--</script>-->
+<script setup lang="ts">
+</script>
