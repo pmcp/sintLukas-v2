233102152d03b69087fb4f3503d0e74e9c09ba6c
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 22:36:54 +0200

    test generate 2

--- pages/[page].vue
+++ pages/[page].vue
@@ -1,5 +1,6 @@
 <template>
   <ContentQuery :path="`${$route.path}.${lang}`" find="one" v-slot="{ data }">
+    {{ data }}
     <ContentRenderer :value="data" class="prose" />
   </ContentQuery>
 </template>
