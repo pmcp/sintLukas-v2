c304e754d7d94135cff8b31e2f2606bb82c74f3f
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 23:21:08 +0200

    test generate 13

--- pages/[page].vue
+++ pages/[page].vue
@@ -1,6 +1,11 @@
 <template>
-  <ContentDoc v-slot="{ doc }">
-    {{ doc }}
+  <ContentDoc>
+    <template v-slot="{ doc }">
+      {{ doc }}
+    </template>
+    <template #not-found>
+      <h1>Document not found</h1>
+    </template>
   </ContentDoc>
 </template>
 <script setup lang="ts">
