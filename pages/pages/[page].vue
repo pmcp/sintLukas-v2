a4adebe89c80e95a79e70c83386c8d0b7fc079ec
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-10 22:32:45 +0200

    stupid folder mistake 4

---
+++ pages/pages/[page].vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    HALLO
+
+  {{ $route.path}} - {{ '/pages'+$route.path }}
+
+    <pre>{{ data }}</pre>
+
+  </main>
+</template>
+<script setup>
+const route = useRoute()
+const { data } = await useAsyncData('page', () => queryContent(route.path).findOne())
+console.log(data)
+</script>
