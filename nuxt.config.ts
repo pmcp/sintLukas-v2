e4c8e4a6289bab280803c09fe8bb0e9c91c85ac5
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-11 00:11:40 +0200

    pivot to build 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/content', "@nuxt/image", '@nuxt/ui'],
+  extends: ['@nuxt/ui-pro'],
   ui: {
     safelistColors: ['black', 'primary', 'secondary']
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
+
+    <ContentList path="/" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <ULink :to="article._path">{{ article.title }}</ULink>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+
   <nuxtLink :to="`/pages/testpage-1.${lang}`">test</nuxtLink>
   --
   <nuxtLink :to="`/testpage-1.${lang}`">test</nuxtLink>
-  -- <nuxtLink to="/about">about</nuxtLink>
+  -- <nuxtLink to="about">about</nuxtLink>
 
 </template>
 
