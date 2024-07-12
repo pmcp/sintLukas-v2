eed48c6ee3728432f86cd1c551e4faf01ae9fc8c
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-12 15:04:01 +0200

    removed pages page

--- pages/pages/[page].vue
+++
@@ -1,9 +0,0 @@
-<template>
-  here we go again - {{ data}}
-  ==== {{ error }}
-</template>
-<script setup lang="ts">
-const route = useRoute()
-const { data, error,  } = await useAsyncData('home', () => queryContent(route.path).findOne())
-
-</script>
